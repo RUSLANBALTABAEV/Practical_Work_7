0726ccdbed2b18d7eef6bcba07beeddeedfdaee4
RUSLANBALTABAEV <63105241+RUSLANBALTABAEV@users.noreply.github.com>
2022-04-30 18:09:27 +0600

    Adding Practical_work_7_Task_2_Listing_7.cpp
    Creating file Practical_work_7_Task_2_Listing_7.cpp

---
+++ Practical_work_7_Task_2_Listing_7.cpp
@@ -0,0 +1,29 @@
+﻿// Practical_work_7_Task_Listing_7.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+//#include <iostream>
+//#include <conio.h>
+
+//using namespace std;
+
+//int main()
+//{
+	//setlocale(LC_ALL, "Russian");
+	//int n, a, S;
+	//cout << "Введите значение n: "; cin >> n;
+
+	//for (int i = 0; i < n * n; i++)
+	//{
+		//cout << "Введите значение a: "; cin >> a;
+
+		//if (a > 0)
+		//{
+			//S += a;
+		//}
+	//}
+
+	//cout << S;
+
+	//_getch();
+	//return 0;
+//}

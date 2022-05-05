c29b22eceb83d73ea118384f31406daac11e2a8e
RUSLANBALTABAEV <63105241+RUSLANBALTABAEV@users.noreply.github.com>
2022-05-05 17:05:59 +0600

    Update Practical_work_7_Task_2_Listing_7.cpp
    Update file Practical_work_7_Task_2_Listing_7.cpp

--- Practical_work_7_Task_2_Listing_7.cpp
+++ Practical_work_7_Task_2_Listing_7.cpp
@@ -1,29 +1,51 @@
-﻿// Practical_work_7_Task_Listing_7.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+﻿// Practical_work_7_Task_2_Listing_7.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
-//#include <iostream>
-//#include <conio.h>
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <conio.h>
 
-//using namespace std;
+using namespace std;
 
-//int main()
-//{
-	//setlocale(LC_ALL, "Russian");
-	//int n, a, S;
-	//cout << "Введите значение n: "; cin >> n;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
 
-	//for (int i = 0; i < n * n; i++)
-	//{
-		//cout << "Введите значение a: "; cin >> a;
+	srand((int)time(0));
+	int m = 0;
+	const int n = 5;
+	double A[n][n], X[(n * n) / 2], s = 0.0;
 
-		//if (a > 0)
-		//{
-			//S += a;
-		//}
-	//}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			A[i][j] = rand() % 19 - 9;
+			
+			if (i > n - 1 - j)
+			{
+				X[m++] = A[i][j];
+			}
 
-	//cout << S;
+			if (A[i][j] > 0)
+			{
+				s += A[i][j];
+			}
 
-	//_getch();
-	//return 0;
-//}
+			cout << setw(3) << A[i][j] << " ";
+		}
+		
+		cout << endl;
+	}
+
+	cout << "Сумма положительный: " << s << endl;
+
+	for (int i = 0; i < (n * n - n) / 2; i++)
+	{
+		cout << X[i] << " ";
+		cout << endl;
+	}
+	_getch();
+	return 0;
+}
